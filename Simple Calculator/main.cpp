5a0646a894a6e37a7cfdc5788d1e89b5fe825203
Randy Odawutey <randyodawutey1221@gmail.com>
2025-08-17 09:33:19 +0000

    Update main.cpp
    Change the operational numbers to sign by change the sign datatype from integer to character

--- Simple Calculator/main.cpp
+++ Simple Calculator/main.cpp
@@ -6,32 +6,28 @@ int main()
 {
     int num1;
     int num2;
-    int sign;
+    char sign;
     cout << "Simple Calculator" << endl;
 
-    cout << "\n----Ooperational Sign----\n";
-        cout << "1. +"<<endl;
-        cout << "2. -"<<endl;
-        cout << "3. *"<<endl;
-        cout << "4. /"<<endl;
+ 
     cout << "Enter first Number: ";
      cin >> num1;
     cout << "Enter second number: ";
      cin >> num2;
-    cout << "Enter operational number: ";
+    cout << "Enter operational sign(+,-,*,/): ";
      cin >> sign;
 
     switch(sign){
-        case 1:
+        case +:
             cout << num1 << " + "<< num2 <<" = "<< num1 + num2 <<endl;
             break;
-        case 2:
+        case -:
             cout << num1 <<" - "<< num2 <<" = "<< num1 - num2 <<endl;
             break;
-        case 3:
+        case *:
             cout << num1 <<" x "<< num2 <<" = "<< num1 * num2 <<endl;
             break;
-        case 4:
+        case /:
             if (num2 != 0){
             cout << num1 <<" / "<< num2 <<" = "<< num1 / num2 <<endl;
             }else{
